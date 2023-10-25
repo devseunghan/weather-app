ffd11b2f713e458acf4d335eeed2ff1b2cda26fa
devseunghan <gankyou@naver.com>
2023-10-25 15:15:38 +0900

    20231025 commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,12 @@ import { getTime } from '@/utils/getTime'
 import RevalidateButton from '@/components/RevalidateButton'
 
 export default async function Home() {
-  const res = await getCurrentWeather('seoul')
-  const time = await getTime(res.location.tz_id)
-
   return (
     <>
       <h1>날씨 앱</h1>
-      <h3>{time.dateTime}</h3>
       <ul className={style.list}>
         <li>
           <Link href="/seoul?name=서울">서울</Link>
-          <span>{res.current.condition.text}</span>
         </li>
         <li>
           <Link href="/NYC?name=뉴욕">뉴욕</Link>
